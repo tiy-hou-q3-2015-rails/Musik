9397cd707cce73ad86b7360a7ddeb81e14a15c52
Christian Neal <c.neal91@gmail.com>
2015-10-19 14:18:59 -0500

    Changed routes so that users#profile is a page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  get 'users/profile'
+  get 'users/:id/profile' => 'users#profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
