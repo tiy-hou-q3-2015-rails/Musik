fb14797ae1215f8208aafe393f0aba219540d40c
Byronj <cekcbj@gmail.com>
2015-10-20 10:23:55 -0500

    fix head changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-
   end
 
   def create
-    @user User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       redirect_to new_subscription_path, notice: "Please subscribe"
@@ -21,5 +20,6 @@ class UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation )
+    params.require(:user).permit(:first_name, :last_name,:username, :email, :password, :password_confirmation )
+  end
 end
