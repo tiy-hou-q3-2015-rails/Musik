48d085389779e6a6af45ffe73a204e71f8a7dc23
Zachary Okuhara <zacharyokuhara@Zacharys-MacBook-Pro.local>
2015-10-22 23:43:14 -0500

    Add order details to confirmation page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,26 +9,23 @@ class SubscriptionsController < ApplicationController
 
   def create
 
-    binding.pry
-    # do any required setup here, including finding or creating the owner object
-    owner = @current_user
-
-    # set your plan in the params hash
-    params[:plan] = Subscription.find_by(id: params[:plan_id])
-
-    # call Payola::CreateSubscription
-    subscription = Payola::CreateSubscription.call(params, owner)
-
-    # Render the status json that Payola's javascript expects
-    render_payola_status(subscription)
+    # binding.pry
+    # # do any required setup here, including finding or creating the owner object
+    # owner = @current_user
+    #
+    # # set your plan in the params hash
+    # params[:plan] = Subscription.find_by(id: params[:plan_id])
+    #
+    # # call Payola::CreateSubscription
+    # subscription = Payola::CreateSubscription.call(params, owner)
+    #
+    # # Render the status json that Payola's javascript expects
+    # render_payola_status(subscription)
   end
 
   def confirmation
     @sale = Payola::Subscription.find_by! guid: params[:sale_guid]
     @subscription = @sale.current_period_end
-
-    #current_user.update current_period_end: current_period_end
-
 
   end
 end
